ae90cfa5a6c1005c72af59abd6a557f75710450e
Lautaro Garcia <lautaro.garcia@intive.com>
2023-09-02 12:45:32 -0300

    Ejemplo SPA con ajax

--- Clase5/ejemplo-SPA-con-ajax/main.js
+++ Clase5/ejemplo-SPA-con-ajax/main.js
@@ -1,16 +1,34 @@
-const title = document.getElementById('title');
-title.innerText = 'Bienvenidos a mi sitio web con AJAX';
+const title = document.getElementById("title");
+const app = document.getElementById("app");
+title.innerText = "Bienvenidos a mi sitio web con AJAX";
 
-const paginaActual = 'index';
-
-switch(paginaActual) {
-    case 'index': 
-        title.innerText = 'index';
-    break;
-    case 'contacto':
-        title.innerText = 'contacto';
-    break;
-    case 'comunidad':
-        title.innerText = 'comunidad';
-    break;
+function mediator() {
+  const paginaActual = location.hash.slice(1);
+  switch (paginaActual) {
+    case "":
+      title.innerText = "index";
+      break;
+    case "contacto":
+      title.innerText = "contacto";
+      break;
+    case "comunidad":
+      title.innerText = "comunidad";
+      break;
+  }
+  cargarPlantilla(paginaActual);
 }
+
+function cargarPlantilla(url) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("get", 'plantillas/' + url + '.html');
+  xhr.addEventListener("load", () => {
+    if (xhr.status == 200) {
+      app.innerHTML = xhr.response;
+    }
+  });
+  xhr.send();
+}
+
+window.addEventListener("popstate", (e) => {
+  mediator();
+});
