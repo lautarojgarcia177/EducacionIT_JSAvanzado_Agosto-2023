923db801740b261b16957d7a9b8d937d04742e0d
Lautaro Garcia <lautarojgarcia177@gmail.com>
2023-10-07 12:42:58 -0300

    resolucion ejercicio 3

--- Clase10/ejercicio3/resolucion.js
+++ Clase10/ejercicio3/resolucion.js
@@ -1,27 +1,42 @@
+/*
+Tienes una caja de regalos de Navidad que Santa Claus quiere entregar a los niños. Cada regalo está representado por una cadena.
+Santa Claus tiene un trineo que puede llevar un peso limitado,
+y cada regalo dentro de la caja tiene un peso que es igual al número de letras en el nombre del regalo.
+Santa Claus también tiene una lista de renos que pueden ayudarlo a entregar los regalos. 
+Cada reno tiene un límite de peso máximo que puede llevar. 
+El límite de peso máximo de cada reno es igual a dos veces el número de letras en su nombre.
+Tu tarea es implementar una función distributeGifts(packOfGifts, reindeers) que recibe una caja de regalos 
+y una lista de renos y devuelve el número máximo de cajas de estos regalos que Santa Claus puede entregar a los niños.
+Las cajas de regalos no se pueden dividir.
+
+Cosas a tener en cuenta:
+
+Las cajas de regalos no se pueden dividir.
+Los nombres de los regalos y los renos siempre serán mayores que 0. 
+*/
+
 function distributeGifts(packOfGifts, reindeers) {
-  const giftsWeight = packOfGifts.map((gift) => gift.length); // Calcula el peso de cada regalo
-  const reindeersCapacity = reindeers.map((reindeer) => 2 * reindeer.length); // Calcula la capacidad de cada reno
-
-  let giftsCount = 0; // Contador de regalos entregados
-  let reindeerIndex = 0; // Índice del reno actual
-
-  for (const giftWeight of giftsWeight) {
-    if (reindeerIndex >= reindeersCapacity.length) {
-      // Si ya se asignaron todos los renos, detén la distribución
-      break;
-    }
-
-    if (giftWeight <= reindeersCapacity[reindeerIndex]) {
-      // Si el regalo puede ser entregado por el reno actual, entrégalo
-      giftsCount += 1;
-      reindeerIndex += 1; // Pasar al siguiente reno
+  // Calcula el peso de cada regalo utilizando length
+  packOfGifts = packOfGifts.map((gift) => gift.length);
+  // Calcula la capacidad de cada reno utilizando length * 2
+  reindeersCapacity = reindeers.map((reinderr) => reinderr.length * 2);
+  // Definir Contador de regalos entregados
+  let contador = 0;
+  // Definir Índice del reno actual
+  let reindeerIndex = 0;
+  for (const giftWeigth of packOfGifts) {
+    // Si el reno tiene capacidad, asignarle el regalo, sino asignarselo al siguiente reno
+    if (reindeersCapacity[reindeerIndex] >= giftWeigth) {
+      contador++;
+      reindeersCapacity[reindeerIndex] -= giftWeigth;
+    } else {
+      reindeerIndex++;
     }
   }
-
-  return giftsCount;
+  return contador;
 }
 
-const packOfGifts = ["book", "doll", "ball"];
-const reindeers = ["dasher", "dancer"];
+const packOfGifts = ["book", "doll", "ball", "bottle"];
+const reindeers = ["thor", "maxi"];
 const result = distributeGifts(packOfGifts, reindeers);
-console.log(result); // 2
+console.log(result); // Me deberia dar 3
