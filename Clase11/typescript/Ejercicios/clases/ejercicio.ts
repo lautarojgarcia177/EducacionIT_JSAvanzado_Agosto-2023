edd1c7ce0b2ffe8030a25313a222d60777bfbb77
Lautaro Garcia <lautaro.garcia@intive.com>
2023-10-14 12:10:34 -0300

    ejercicio clases

---
+++ Clase11/typescript/Ejercicios/clases/ejercicio.ts
@@ -0,0 +1,30 @@
+/*
+ Debo armar una base de datos en memoria de la gente que compone mi fabrica.
+ Todos tienen nombre, cargo y edad
+ Hay 2 tipos:
+ 1) Operarios, tienen herramienta
+ 2) Encargados, tienen titulo
+ Actualmente mi base de datos esta Hardcodeada, modificarla de forma que se a creada respetando tipos
+
+*/
+
+const genteFabrica = [
+    {
+        nombre: 'Pepe',
+        cargo: 'Operario',
+        edad: 25,
+        herramienta: 'Sierra'
+    },
+    {
+        nombre: 'Pipo',
+        cargo: 'Encargado',
+        edad: 30,
+        titulo: 'Ingeniero industrial'
+    },
+];
+
+// Posibilidad de hacerlo con tipos,
+
+// Posibilidad de hacerlo con interfaces,
+
+// Posibilidad de hacerlo con clases,
