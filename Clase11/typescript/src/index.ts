f9dc1f7d8a116d5ac852b226aea17c6f70871c4f
Lautaro Garcia <lautaro.garcia@intive.com>
2023-10-14 11:03:18 -0300

    seteado el proyecto typescript

---
+++ Clase11/typescript/src/index.ts
@@ -0,0 +1,7 @@
+console.log("hola mundo");
+console.log("hola mundo");
+console.log("hola mundo");
+console.log("hola mundo");
+console.log("hola mundo");
+console.log("hola mundo");
+console.log("hola mundo");
